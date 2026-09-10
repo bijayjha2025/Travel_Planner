fee70587f2e87f7798111c4ad2eaa39780b93fbf
Bijay Jha <bijayjha12345@gmail.com>
2026-09-10 17:37:36 +0545

    initial navbar setup

---
+++ frontend/src/components/layout/navbar.jsx
@@ -0,0 +1,25 @@
+export const Navbar = () => {
+
+    return(
+     <nav className='sticky top-0 z-50 bg-transparent backdrop-blur-md border-b border-sand'>
+      <div className='max-w-7xl mx-auto px-6 h-20 flex items-center justify-between'>
+
+      <div>
+       <a>Destinations</a>
+       <a>Experiences</a>
+       <a>Hidden Gems</a>
+       <a>Responsible Travel</a>
+      </div>
+
+      <div>
+       <a>Sign In</a>
+       <a>Plan My Journey</a>
+      </div>
+     
+      <button>
+
+      </button>
+      </div>
+     </nav>
+    )
+}
